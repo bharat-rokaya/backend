8d2db30da0a9d3f7c8234b62efd8c29fc4255167
bharat rokaya <bharat12rokaya123@gmail.com>
2026-04-29 16:31:49 +0545

    first commit

--- src/server.js
+++ src/server.js
@@ -3,25 +3,21 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('hello from express');
+    res.send('Hello from express');
 });
 
 app.get('/products', (req, res) => {
-    res.send('Products page');
+    res.send('This is the products page');
 });
 
 app.get('/about', (req, res) => {
-    res.send('About page');
-});
-
-app.get('/contact', (req, res) => {
-    res.send('Contact page');
+    res.send('This is the about page');
 });
 
 app.post('/contact', (req, res) => {
     res.send('Contact form submitted');
-})
+});
 
 app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
+    console.log('Server is running on port 3000');
 });
