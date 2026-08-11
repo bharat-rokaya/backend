1f91ffea5bd58dcf27efa72ee9ebfb466a83436e
satoshi <bharatrokaya2007@gmail.com>
2026-08-11 09:11:45 +0545

    config

--- src/config/config.js
+++ src/config/config.js
@@ -5,6 +5,7 @@ dotenv.config();
 const config = {
     port: process.env.PORT || 3000,
     mongoUrl: process.env.MONGO_URL || "",
+    jwtSecret: process.env.JWT_SECRET || ""
 };
 
 export default config;
